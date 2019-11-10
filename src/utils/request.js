e7bbb79fbfdaabc8118046ea39b3e13090d8c3f8
Kain Shi <a904616537@outlook.com>
2019-11-10 23:54:41 +0800

    _

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL : 'http://service.workspace.h-fish.vip',
+  baseURL : 'http://server.workzspace.cn',
   // baseURL : 'http://127.0.0.1:8481',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
